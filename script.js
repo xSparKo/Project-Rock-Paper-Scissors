b002917426c95600297bc9b9bc91b1c0745bbc36
SparKo <107945330+xSparKo@users.noreply.github.com>
2024-01-01 19:31:18 +0100

    Add player input and game logic
    Added player input functionality, allowing the user to choose between "Rock," "Paper," or "Scissors." The playRound function determines the winner based on the player's selection and the computer's choice. The game now displays the outcome of the round.

--- script.js
+++ script.js
@@ -4,10 +4,36 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3)
     if (randomNumber === 0) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
+
+let playerSelection = (prompt("Choose one: Rock, Paper or Scissors"))
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    //Checks if its a TIE
+    if (playerSelection === computerSelection) {
+        return "It's a tie! Replay the round.";
+    }
+
+    //Determine the winner and returns it with text
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+    } else {
+        return `You Lose! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} looses against ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+    }
+}
+
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
